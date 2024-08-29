88bddd1e3c62abf03d996fe5db5d282805e8eb36
santiago <124795721+santttimunoz@users.noreply.github.com>
2024-08-28 22:39:37 -0500

    ejercicio de loteria

--- src/javanotes/Conditional.java
+++ src/javanotes/Conditional.java
@@ -1,20 +1,18 @@
 package javanotes;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Conditional {
     public  static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        int acumulador = 0;
+        int contador = 0;
         int [] numeros = new int[6];
         int [] numerosUsuarios = new int[6];
 
         for(int i = 0 ; i < numeros.length; i++){
-            int ran = (int)(Math.random()*10+1);
+            int ran = (int)(Math.random()*100+1);
             numeros[i] = ran;
         }
        // for (int i = 0 ; i < numeros.length; i++){
@@ -28,14 +26,50 @@ public class Conditional {
        // for(int i = 0 ; i < numerosUsuarios.length; i++){
          //   System.out.println("numeros ingresados: " + numerosUsuarios[i] + "\n");
        // }
+        if(!Arrays.equals(numeros, numerosUsuarios)){
+            Map<Integer, Integer> numerosRepetidos = new HashMap<>();
 
-        for(int i = 0 ; i < numerosUsuarios.length; i++){
-            if(numerosUsuarios[i] == numeros[1] || numerosUsuarios[i] == numeros[2] || numerosUsuarios[i] == numeros[3]
-                    || numerosUsuarios[i] == numeros[4] || numerosUsuarios[i] == numeros[0]){
-
+            for(int num : numeros){
+                numerosRepetidos.put(num, numerosRepetidos.getOrDefault(num, 0) + 1);
             }
-        for(int j = 0 ; j < numerosUsuarios.length; j++){
-                System.out.println(numeros[j]);
+
+            for(int num : numerosUsuarios){
+                numerosRepetidos.put(num, numerosRepetidos.getOrDefault(num, 0) + 1);
+            }
+
+            for(int contar : numerosRepetidos.values()){
+                if(contar > 1){
+                    contador++;
+                }
+            }
+
+            System.out.println("");
+            System.out.print("numeros ganadores: ");
+            for(int num : numeros){
+                System.out.print(num + ", ");
+            }
+
+            System.out.println("");
+            System.out.print("numeros ingresados: ");
+            for(int num : numerosUsuarios){
+                System.out.print(num + ", ");
+            }
+            System.out.println("");
+            System.out.print("coincidencias: " + contador +"\n");
+
+            if(contador < 4){
+                System.out.println("lo siento, no has ganado nada");
+            }
+            else if(contador == 4){
+                System.out.print("recuperas el valor del ticket");
+            } else if (contador == 5) {
+                System.out.print("felicidades has ganado el premio de 100.000");
+            }
+
+        }else{
+            System.out.print("felicidades has ganado el premio mayor de 1.000.000");
         }
+
     }
 }
+
